77b5d20aadef90273e603323ef19a3ff0b65a066
sarah-svg <kjskillen@gmail.com>
2021-02-08 14:38:09 -0800

    fetch api get and getOne characters

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -10,3 +10,16 @@ export const getCharacters = () => {
       }))
     );
 };
+export const getoneCharacter = (character) => {
+  return fetch(`https://rickandmortyapi.com/api/character/${character}`)
+    .then((res) => res.json())
+    .then((character) =>
+      character.map(({ name, species, type, image }) => ({
+        name,
+        species,
+        text: type, 
+        image,
+      }))
+    );
+};
+  
